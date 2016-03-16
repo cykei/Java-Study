22779753f6cea2b22040f80c14db900162afb871
yeondd <garnet_22@naver.com>
2016-03-16 18:01:24 +0900

    wk 3 at uni

---
+++ src/headfirst/strategy/Duck.java
@@ -0,0 +1,27 @@
+package headfirst.strategy;
+
+public abstract class Duck{
+  FlyBehavior flyBehavior;
+  QuackBehavior quackBehavior;
+  
+  public Duck(){}
+  public void setFlyBehaivor (FlyBehavior fb){
+    flyBehavior = fb;
+  }
+ public void setQuackBehavior(QuackBehavior qb) { 
+   quackBehavior = qb; 
+  } 
+ abstract void display(); 
+  
+ public void performFly() { 
+   flyBehavior.fly(); 
+  } 
+   
+  public void performQuack() { 
+   quackBehavior.quack(); 
+  } 
+   
+  public void swim() { 
+   System.out.println("All ducks swim!"); 
+  } 
+ } 
